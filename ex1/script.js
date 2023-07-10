d789b98dc2a92f0295fc64e2c3337dd5fd503e07
SebasJacome <0237105@up.mx>
2023-07-10 13:19:49 -0600

    Adding EventHandlers and modifying the first exercise

--- ex1/script.js
+++ ex1/script.js
@@ -13,3 +13,19 @@ console.log({
     pid,
     input,
 });
+
+// h1.innerText = 'Yes king <br> Amazing'
+// let variable = h1.getAttribute('pantalla')
+// console.log(variable);
+// h1.setAttribute('class')
+h1.classList.add('rojo')
+h1.classList.remove('verde')
+// h1.classList.toggle('verde')
+// h1.classList.contains('verde')
+
+input.value = ' '
+input.placeholder = 'Introduzca un numero'
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://static.wikia.nocookie.net/pokemon-opalo-por-ericlostie/images/3/34/094.png/revision/latest?cb=20220313114504&path-prefix=es')
+pid.replaceWith(img);
